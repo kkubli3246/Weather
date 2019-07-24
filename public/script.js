4498817aa6ed6e37e1e81478d0e47f8a7018d103
Kekoa Kubli <kkubli3246@gmail.com>
2019-07-24 12:15:47 -0700

    Complete API integration

--- public/script.js
+++ public/script.js
@@ -31,9 +31,10 @@ searchBox.addListener('places_changed', () =>{
     const windElement = document.querySelector('[data-wind]');
     const percipitationElement = document.querySelector('[data-precipitation]');
     const temperatureElement = document.querySelector('[data-temperature]');
+    setWeatherColor('clear-day');
     icon.set('icon', 'clear-day');
     icon.play();
-    
+
     function setWeatherData(data, place){
         locationElement.textContent = place;
         statusElement.textContent = data.summary;
@@ -41,6 +42,42 @@ searchBox.addListener('places_changed', () =>{
         percipitationElement.textContent = data.precipProbability * 100 + '%';
         windElement.textContent = data.windSpeed;
         icon.set('icon', data.icon)
+        setWeatherColor(data.icon);
         icon.play();
+    }
+
+    function setWeatherColor(icon){
+        switch(icon){
+            case 'clear-day':
+                document.body.style.background = "linear-gradient(rgb(77, 148, 255),skyblue)";
+                break;
+            case 'partly-cloudy-day':
+                document.body.style.background = "linear-gradient(white,skyblue)";
+                break;
+            case 'clear-night':
+                document.body.style.background = "linear-gradient(rgb(77, 148, 255),navy)";
+                break;
+            case 'partly-cloudy-night':
+                document.body.style.background = "linear-gradient(white,skyblue)";
+                break;    
+            case 'cloudy':
+                document.body.style.background = "linear-gradient(darkgray,white)";
+                break;
+            case 'rain':
+                document.body.style.background = "linear-gradient(darkgray,gray)";
+                break; 
+            case 'sleet':
+                document.body.style.background = "linear-gradient(gray,skyblue)";
+                break;
+            case 'snow':
+                document.body.style.backgroundColor = "white";
+                break;
+            case 'wind':
+                document.body.style.background = "linear-gradient(lightgray,skyblue)";
+                break;
+            case 'fog':
+                document.body.style.backgroundColor = "gray";
+                break;  
+        }
     }
 
